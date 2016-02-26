775e7bd1a1e4d50b94254f52987ac9390899fecc
Otávio Fonseca <otaviopfonseca@yahoo.com.br>
2016-02-26 18:31:28 -0300

    Configuração inicial da solution

---
+++ Roda/Roda.Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roda.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Roda/Roda.Component/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roda.Component
+{
+    public class Class1
+    {
+    }
+}
