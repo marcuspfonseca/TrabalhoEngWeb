aff482909c05229c4cbf6397221c71783fbc6439
Otávio Fonseca <otaviopfonseca@yahoo.com.br>
2016-02-27 15:15:56 -0300

    Criação Views Iniciais

---
+++ Roda/Roda.Web/Controllers/JogosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roda.Web.Controllers
+{
+    public class JogosController : Controller
+    {
+        // GET: Jogos
+        public ActionResult Index()
+        {
+            return View("~/Views/Roda/Sobre.cshtml");
+        }
+    }
+}
