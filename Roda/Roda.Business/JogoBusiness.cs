525a1ccbd097c778b0bcd9e188df41eee5cbce1c
Otávio Fonseca <otaviopfonseca@yahoo.com.br>
2016-02-27 15:39:13 -0300

    Arquivo roda business

---
+++ Roda/Roda.Business/JogoBusiness.cs
@@ -0,0 +1,30 @@
+﻿using Roda.Business.Conversor;
+using Roda.DataAccess;
+using Roda.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roda.Business
+{
+    public class JogoBusiness
+    {
+        private JogoDataAccess _jogoDataAcess;
+        private JogoDataAccess JogoDataAccess
+        {
+            get
+            {
+                if (_jogoDataAcess == null)
+                    _jogoDataAcess = new JogoDataAccess();
+                return _jogoDataAcess;
+            }
+        }
+        public List<JogoEntity> listarJogosCadastrados()
+        {
+            List<Jogo> jogos = JogoDataAccess.listarJogos();
+            return ConversorModelEntity.ConverterJogoModelParaJogoEntity(jogos);
+        }
+    }
+}
